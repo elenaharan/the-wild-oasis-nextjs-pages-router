be937b54fc730de3b1c9a3f314c76db3900f6289
Elena <elenaharan@gmail.com>
2024-10-03 06:09:27 +0100

    fetch data from the server with getStaticProps

--- pages/cabins.js
+++ pages/cabins.js
@@ -1,4 +1,12 @@
-function Cabins() {
+import { getCabins } from "@/lib/data-service";
+
+export async function getStaticProps() {
+  const cabins = await getCabins();
+  return { props: { cabins } };
+}
+
+function Cabins({ cabins }) {
+  console.log(cabins);
   return <div>Cabins page</div>;
 }
 
