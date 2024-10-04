0fab437eba0787605db9e974f10a2833e0dd4e04
Elena <elenaharan@gmail.com>
2024-10-04 07:49:19 +0100

    add a handler for contact endpoint

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,29 @@
+import { supabase } from "@/lib/supabase";
+
 export default async function handler(req, res) {
-  res.status(200).json({ data: "TEST" });
+  if (req.method !== "POST")
+    res.status(405).json({
+      success: false,
+      message: "Please make a POST request.",
+    });
+
+  const contactData = {
+    fullName: "Elena",
+    email: "test",
+    subject: "booking",
+    message: "Hey!",
+  };
+
+  const { error } = await supabase.from("contact").insert([contactData]);
+
+  if (error)
+    res.status(500).json({
+      success: false,
+      message: "Could not send your message. Please try again.",
+    });
+
+  res.status(200).json({
+    success: true,
+    message: "Thanks for your message! We will be in touch soon :)",
+  });
 }
