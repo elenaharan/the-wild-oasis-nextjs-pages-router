bfea78f30d193c4bace1f587a949fe8c004ddabb
Elena <elenaharan@gmail.com>
2024-10-05 15:45:21 +0100

    handle form submission

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -7,12 +7,7 @@ export default async function handler(req, res) {
       message: "Please make a POST request.",
     });
 
-  const contactData = {
-    fullName: "Elena",
-    email: "test",
-    subject: "booking",
-    message: "Hey!",
-  };
+  const contactData = JSON.parse(req.body);
 
   const { error } = await supabase.from("contact").insert([contactData]);
 
